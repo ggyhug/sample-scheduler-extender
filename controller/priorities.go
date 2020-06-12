e8fbd027e8aa563cda253b62d6df284aaa46d765
guogy <50547999+ggyhug@users.noreply.github.com>
2020-06-12 10:20:01 +0800

    Update priorities.go

--- controller/priorities.go
+++ controller/priorities.go
@@ -2,23 +2,32 @@ package controller
 
 import (
 	"log"
+	"time"
 
 	schedulerapi "k8s.io/kubernetes/pkg/scheduler/api"
 )
 
+// It'd better to only define one custom priority per extender
+// as current extender interface only supports one single weight mapped to one extender
+// and also it returns HostPriorityList, rather than []HostPriorityList
+
 const (
-	PrioMsg = "pod %v/%v gets score %v\n"
+	// lucky priority gives a random [0, schedulerapi.MaxPriority] score
+	// currently schedulerapi.MaxPriority is 10
+	luckyPrioMsg = "pod %v/%v is lucky to get score %v\n"
 )
 
-//打分，排序
+// it's webhooked to pkg/scheduler/core/generic_scheduler.go#PrioritizeNodes()
+// you can't see existing scores calculated so far by default scheduler
+// instead, scores output by this function will be added back to default scheduler
 func prioritize(args schedulerapi.ExtenderArgs) *schedulerapi.HostPriorityList {
 	pod := args.Pod
 	nodes := args.Nodes.Items
 
 	hostPriorityList := make(schedulerapi.HostPriorityList, len(nodes))
 	for i, node := range nodes {
-		score := i+1
-		log.Printf(PrioMsg, pod.Name, pod.Namespace, score)
+		score:=rand.Intn % 4;
+		log.Printf(luckyPrioMsg, pod.Name, pod.Namespace, score)
 		hostPriorityList[i] = schedulerapi.HostPriority{
 			Host:  node.Name,
 			Score: score,
